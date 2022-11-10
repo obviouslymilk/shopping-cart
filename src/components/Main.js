54c962e9faa6823c85cee0427254741411bb1da0
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-10 12:19:32 +1000

    Add home and store navigation for routing testing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from '../containers/Home';
+import Store from '../containers/Store'
+
 export default function Main() {
     return <main>
-        
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+        </Routes>
     </main>
 }
