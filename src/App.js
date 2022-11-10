0c7eff3fedc765adfd2a27a0782471b0edd8994a
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-10 11:36:09 +1000

    Create basic layout of App components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
