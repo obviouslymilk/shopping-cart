b6e773e84ce5c36e740d2d89ff3809c8543d6d9c
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-11 10:19:56 +1000

    Add Cart to routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from '../containers/Home';
 import Store from '../containers/Store'
+import Cart from '../containers/Cart'
 
 export default function Main() {
     return <main>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/store" element={<Store />} />
+            <Route path="/cart" element={<Cart />} />
         </Routes>
     </main>
 }
