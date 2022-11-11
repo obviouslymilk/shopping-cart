3a1ef498c47ab20d0c0a417f0cd47d6ff4c45c93
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-11 11:25:15 +1000

    Add products routing

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from '../containers/Home';
 import Store from '../containers/Store'
 import Cart from '../containers/Cart'
+import data from '../data/products.json';
+import { useState } from "react";
+import Product from "../containers/Product";
 
 export default function Main() {
+    const [products, setProducts] = useState(data.products);
+
     return <main>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Store />} />
+            <Route index element={<Home />} />
+            <Route path="/store" element={<Store data={products} />} />
+            <Route path="/product/:id" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
         </Routes>
     </main>
