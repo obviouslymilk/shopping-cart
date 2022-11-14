1404c416409f47e8bf490ed0e775001b4c7cceda
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-14 10:18:59 +1000

    Add handleAddToCart function

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,14 +5,26 @@ import Cart from '../containers/Cart'
 import data from '../data/products.json';
 import { useState } from "react";
 import Product from "../containers/Product";
+import { findById } from "../utils/Utils";
 
 export default function Main() {
+
+    const [cart, setCart] = useState({});
+
+    const handleAddToCart = (e) => {
+        const id = e.target.dataset.id;
+        setCart({
+            ...cart,
+            [id]: (cart[id] || 0) + 1
+        })
+    }
+
     return <main>
         <Routes>
             <Route index element={<Home />} />
-            <Route path="store" element={<Store data={data.products} />} />
-            <Route path="product/:id" element={<Product data={data.products} />} />
-            <Route path="cart" element={<Cart />} />
+            <Route path="store" element={<Store data={data.products} onAdd={handleAddToCart} />} />
+            <Route path="product/:id" element={<Product data={data.products} onAdd={handleAddToCart} />} />
+            <Route path="cart" element={<Cart cart={cart} />} />
         </Routes>
     </main>
 }
