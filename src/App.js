ede1281477fb9f5f864f4f20a6a866bb90024a90
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-15 09:45:22 +1000

    Add css App layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Main from './components/Main';
 import Footer from './components/Footer';
 import { BrowserRouter } from "react-router-dom";
+import './styles/App.scss'
 
 function App() {
   return (
