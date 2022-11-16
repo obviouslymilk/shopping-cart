c1662e7dbed4cd29eb11589bcd72930427d88f85
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-16 12:11:17 +1000

    Add GitHub link to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import '../styles/Footer.scss'
+import github from '../assets/github.png'
+
 export default function Footer() {
     return <footer id="footer">
-        LEGO, the LEGO logo, the Minifigure, DUPLO, LEGENDS OF CHIMA, NINJAGO, BIONICLE, MINDSTORMS and MIXELS are trademarks and copyrights of the LEGO Group. ©2022 The LEGO Group.<br />All rights reserved.
-        <br />
-        <strong>THIS WEB SITE WAS MADE FOR WEB-DEVELOPMENT PRACTICE</strong>
+        LEGO, the LEGO logo, the Minifigure, DUPLO, LEGENDS OF CHIMA, NINJAGO, BIONICLE, MINDSTORMS and MIXELS are trademarks and copyrights of the LEGO Group. ©2022 The LEGO Group.<br /> THIS WEB SITE WAS MADE FOR WEB-DEVELOPMENT PRACTICE
+        <a href="https://github.com/obviouslymilk/shopping-cart"><img src={github} alt='github'></img></a>
     </footer>
 }
