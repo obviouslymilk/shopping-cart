b3af7a130556335c22f802a7c8d527d3fc8be46b
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-16 11:32:52 +1000

    Add footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
+import '../styles/Footer.scss'
 export default function Footer() {
-    return <footer>
-        
+    return <footer id="footer">
+        LEGO, the LEGO logo, the Minifigure, DUPLO, LEGENDS OF CHIMA, NINJAGO, BIONICLE, MINDSTORMS and MIXELS are trademarks and copyrights of the LEGO Group. ©2022 The LEGO Group.<br />All rights reserved.
+        <br />
+        <strong>THIS WEB SITE WAS MADE FOR WEB-DEVELOPMENT PRACTICE</strong>
     </footer>
 }
