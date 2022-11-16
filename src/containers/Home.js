fbc8d4b67240ed7aa59890ea53764bd19e78a385
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-16 18:01:23 +1000

    Remove button from Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,8 @@
+import { Link } from "react-router-dom";
 import "../styles/Home.scss";
 
 export default function Home() {
     return <div id="home">
         <h1>Welcome!</h1>
-        <button>Start shopping</button>
     </div>
 }
